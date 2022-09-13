866f606e8bdee3e6f10ce02fff428188f4ccd81d
Max Vasiliev <max@vasiliev.me>
2022-09-14 00:42:56 +0300

    A spiral tower builder now works

--- src/dtower/core.clj
+++ src/dtower/core.clj
@@ -126,7 +126,7 @@
 
 
 (def directions
-  (cycle [:up :right :down :forward :left :up :right :forward :down :left :up :forward :right :down :left :forward]))
+  (cycle [:forward :up :right :down :forward :left :up :right :forward :down :left :up :forward :right :down :left]))
 
 
 (defn next-word-point [point word dir]
@@ -134,9 +134,82 @@
     (case dir
       :right (update point :x + length)
       :left (update point :x - length)
-      :up (update point :y + length)
-      :down (update point :y - length)
+      :up (update point :y - length)
+      :down (update point :y + length)
       :forward (update point :z + length))))
+
+
+(def WORD_MAX_LEN 7)
+
+
+(defn get-word-up [word used-words]
+  (let [letter (str (first word))]
+    (first (filter (fn [cand]
+                     (and (str/ends-with? cand letter)
+                          (not (contains? used-words cand))))
+                   found))))
+
+(defn get-word-down [word used-words]
+  (let [letter (str (last word))]
+    (first (filter (fn [cand]
+                     (and (str/starts-with? cand letter)
+                          (not (contains? used-words cand))))
+                   found))))
+
+(defn get-word-left [word used-words]
+  (let [letter (str (last word))]
+    (first (filter (fn [cand]
+                     (and (str/ends-with? cand letter)
+                          (not (contains? used-words cand))))
+                   found))))
+
+(defn get-word-right [word used-words]
+  (let [letter (str (first word))]
+    (first (filter (fn [cand]
+                     (and (str/starts-with? cand letter)
+                          (not (contains? used-words cand))))
+                   found))))
+
+(defn get-word-forward [word used-words]
+  (let [letter (str (first word))]
+    (first (filter (fn [cand]
+                     (and (str/ends-with? cand letter)
+                          (not (contains? used-words cand))))
+                   found))))
+
+(defn get-next-word [word used-words dir prev-dir]
+  (if (= :forward prev-dir)
+    (case dir
+      :up (get-word-up word used-words)
+      :down (get-word-right word used-words)
+      :left (get-word-up word used-words)
+      :right (get-word-right word used-words)
+      :forward (case prev-dir
+                 :up (get-word-forward word used-words)
+                 :down (get-word-left word used-words)
+                 :left (get-word-forward word used-words)
+                 :right (get-word-left word used-words)
+                 :forward (get-word-forward word used-words)
+                 nil (get-word-forward word used-words)))
+
+    (case dir
+      :up (get-word-up word used-words)
+      :down (get-word-down word used-words)
+      :left (get-word-left word used-words)
+      :right (get-word-right word used-words)
+      :forward (case prev-dir
+                 :up (get-word-forward word used-words)
+                 :down (get-word-left word used-words)
+                 :left (get-word-forward word used-words)
+                 :right (get-word-left word used-words)
+                 :forward (get-word-forward word used-words)
+                 nil (get-word-forward word used-words)))))
+
+(-> "затейщик"
+    (get-next-word #{} :down)
+    (get-next-word #{} :left)
+    (get-next-word #{} :forward)
+    )
 
 
 (defn get-words [n]
@@ -146,8 +219,14 @@
          result     []
          used-words #{"затейщик" "куконица" "скупщица"}]
     (let [dir        (nth directions dir-n)
-          next-word  (get-next-start word used-words)
+          prev-dir   (if (= 0 dir-n)
+                       nil
+                       (nth directions (dec dir-n)))
+          next-word  (get-next-word word used-words dir prev-dir)
           next-word' (split-word next-word point dir)]
+
+      (when-not next-word
+        (throw (Exception. (str "Can not find a word to continue. Last word used: " word ". Direction: " dir ". Iteration: " dir-n))))
 
       (if (= dir-n n)
         result
@@ -158,40 +237,65 @@
                (conj used-words next-word))))))
 
 (def result
-  (distinct (flatten (get-words 50))))
+  (distinct (flatten (get-words 100))))
 
 
 
 (def test-1
-  {:towerId "3"
-   :letters (concat
-             (distinct [{:name "з" :x 0 :y 0 :z 0}
-                        {:name "а" :x 1 :y 0 :z 0}
-                        {:name "т" :x 2 :y 0 :z 0}
-                        {:name "е" :x 3 :y 0 :z 0}
-                        {:name "й" :x 4 :y 0 :z 0}
-                        {:name "щ" :x 5 :y 0 :z 0}
-                        {:name "и" :x 6 :y 0 :z 0}
-                        {:name "к" :x 7 :y 0 :z 0}
+  {:towerId "10"
+   :letters (distinct
+             (concat
+              (distinct [{:name "з" :x 0 :y 0 :z 0}
+                         {:name "а" :x 1 :y 0 :z 0}
+                         {:name "т" :x 2 :y 0 :z 0}
+                         {:name "е" :x 3 :y 0 :z 0}
+                         {:name "й" :x 4 :y 0 :z 0}
+                         {:name "щ" :x 5 :y 0 :z 0}
+                         {:name "и" :x 6 :y 0 :z 0}
+                         {:name "к" :x 7 :y 0 :z 0}
 
-                        {:name "к" :x 7 :y 0 :z 0}
-                        {:name "у" :x 7 :y 1 :z 0}
-                        {:name "к" :x 7 :y 2 :z 0}
-                        {:name "о" :x 7 :y 3 :z 0}
-                        {:name "н" :x 7 :y 4 :z 0}
-                        {:name "и" :x 7 :y 5 :z 0}
-                        {:name "ц" :x 7 :y 6 :z 0}
-                        {:name "а" :x 7 :y 7 :z 0}
+                         {:name "к" :x 7 :y 0 :z 0}
+                         {:name "у" :x 7 :y 1 :z 0}
+                         {:name "к" :x 7 :y 2 :z 0}
+                         {:name "о" :x 7 :y 3 :z 0}
+                         {:name "н" :x 7 :y 4 :z 0}
+                         {:name "и" :x 7 :y 5 :z 0}
+                         {:name "ц" :x 7 :y 6 :z 0}
+                         {:name "а" :x 7 :y 7 :z 0}
 
-                        {:name "с" :x 0 :y 7 :z 0}
-                        {:name "к" :x 1 :y 7 :z 0}
-                        {:name "у" :x 2 :y 7 :z 0}
-                        {:name "п" :x 3 :y 7 :z 0}
-                        {:name "щ" :x 4 :y 7 :z 0}
-                        {:name "и" :x 5 :y 7 :z 0}
-                        {:name "ц" :x 6 :y 7 :z 0}
-                        {:name "а" :x 7 :y 7 :z 0}])
-             result)})
+                         {:name "с" :x 0 :y 7 :z 0}
+                         {:name "к" :x 1 :y 7 :z 0}
+                         {:name "у" :x 2 :y 7 :z 0}
+                         {:name "п" :x 3 :y 7 :z 0}
+                         {:name "щ" :x 4 :y 7 :z 0}
+                         {:name "и" :x 5 :y 7 :z 0}
+                         {:name "ц" :x 6 :y 7 :z 0}
+                         {:name "а" :x 7 :y 7 :z 0}])
+              result))})
+
+
+(defn get-tower-status [tower-id]
+  (->
+   (http/get (str "https://dtower-api.datsteam.dev/towers/" tower-id)
+             {:throw-exceptions false
+              :headers          {"Content-Type" "application/json"
+                                 "token"        "cuKVhArj7VeNfjWVfdcp/Bf/PZOf+WEZBdNZ14554HOQawZKrvMD7iqA8X1PpwXA7hM=\n"}})
+   :body))
+
+
+(defn top []
+  (-> (http/get "https://dtower-api.datsteam.dev/towers/top"
+                {:throw-exceptions false
+                 :headers          {"Content-Type" "application/json"
+                                    "token"        "cuKVhArj7VeNfjWVfdcp/Bf/PZOf+WEZBdNZ14554HOQawZKrvMD7iqA8X1PpwXA7hM=\n"}})
+      :body
+      (charred/read-json :key-fn keyword)
+      :leaderboard))
+
+
+(get-tower-status (:towerId test-1))
+
+(top)
 
 (comment
  (http/post "https://dtower-api.datsteam.dev/towers"
@@ -202,19 +306,7 @@
 
 
  ;; tower result
- (->
-  (http/get "https://dtower-api.datsteam.dev/towers/3"
-            {:throw-exceptions false
-             :headers          {"Content-Type" "application/json"
-                                "token"        "cuKVhArj7VeNfjWVfdcp/Bf/PZOf+WEZBdNZ14554HOQawZKrvMD7iqA8X1PpwXA7hM=\n"}})
-  :body)
+ 
 
 
- (def top
-   (-> (http/get "https://dtower-api.datsteam.dev/towers/top"
-                 {:throw-exceptions false
-                  :headers          {"Content-Type" "application/json"
-                                     "token"        "cuKVhArj7VeNfjWVfdcp/Bf/PZOf+WEZBdNZ14554HOQawZKrvMD7iqA8X1PpwXA7hM=\n"}})
-       :body
-       (charred/read-json :key-fn keyword)
-       :leaderboard)))
+ )
